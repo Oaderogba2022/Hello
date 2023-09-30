21219f7ddd61e01f55e84b24280157bc7c1e4c3b
Olamide Aderogba <G00401971@atu.ie>
2023-09-30 17:12:33 +0100

    Used GetMapping to print Hello World on port 8080.

--- src/main/java/ie/atu/hello/HelloController.java
+++ src/main/java/ie/atu/hello/HelloController.java
@@ -7,4 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello, World!";
+
+    }
+
 }
